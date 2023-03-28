9e674bdc0903cfff9b527300d51340245079351a
Petrando <bangkitwisata@gmail.com>
2023-03-28 21:49:26 +0800

    install tailwind

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
