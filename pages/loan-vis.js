cba8a094262e2bb8f6fa3259987b6e9e7af57f0f
Petrando <bangkitwisata@gmail.com>
2023-03-29 13:06:07 +0800

    added tooltip to loan vis page

--- pages/loan-vis.js
+++ pages/loan-vis.js
@@ -1,12 +1,14 @@
 import { Layout } from '../components/layout';
 import Image from 'next/image';
+import 'react-tooltip/dist/react-tooltip.css';
+import { Tooltip } from 'react-tooltip';
 
 export default function LoanVis() {    
     
     return (
         <Layout alwaysFitScreen={true}>
             
-            <div className='w-screen h-full flex-grow bg-sky-200 relative'>
+            <div className='picContainer w-screen h-full flex-grow bg-sky-200 relative'>
                 <a target="_blank" href="https://viscene.vercel.app/loan-vis" rel="noopener noreferrer">    
                     <Image
                         alt='Mountains'
@@ -15,6 +17,9 @@ export default function LoanVis() {
                     />
                 </a>
             </div>
+            <Tooltip anchorSelect=".picContainer" place="top">
+                Screenshot only. Click to visit.
+            </Tooltip>
         </Layout>
     );
 }
